8f29efbfdbbc19c72c78723bddcf333cd30149b0
peter <psarateanu@hotmail.ca>
2021-11-12 10:09:42 -0500

    commit remise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Form} from './appForm';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'intra';
+  
+   add(nbItems:number) {
+    for (let i = 0; i < nbItems; i++) {
+      
+    }
+  }
+
+  submitted = false;
+
+  onSubmit() { this.submitted = true; }
+
 }
---
+++ src/app/appForm.ts
@@ -0,0 +1,9 @@
+export class Form {
+    constructor(
+      public menuElements: number,
+      public sousMenuElements: number,
+      public imageUrl: string,
+      public couleurNav: string,
+      public enHautBas: boolean
+  ) {}
+}
